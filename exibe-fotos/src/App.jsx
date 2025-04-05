81a6946e92413d924c55d37d902ca100d17864d1
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-04 22:34:27 -0300

    learn(ap07-react): encerra a secao 2.13
    - ja fizemos uso da axios
    - falta tornar a exibicao das fotos responsiva
    - tambem vamos implementar um back end a fim de isolar a chave de api

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -2,35 +2,57 @@
 import React from 'react'
 import Busca from './components/Busca'
 import env from 'react-dotenv'
-import { createClient } from 'pexels'
+// import { createClient } from 'pexels'
+import pexelsClient from './utils/pexelsClient'
+import Imagem from './components/Imagem'
+import ListaImagens from './components/ListaImagens'
+import PexelsLogo from './components/PexelsLogo'
 
-class App extends React.Component{
+class App extends React.Component {
 
   pexelsClient = null
 
-  //assim que o componente terminar de ser montado na árvore, construir o objeto com createClient, tal qual visto na documentação
-  componentDidMount(){
-    this.pexelsClient = createClient(env.PEXELS_KEY)
-    console.log(this.pexelsClient)
+  state = {
+    photos: []
   }
 
+  //assim que o componente terminar de ser montado na árvore, construir o objeto com createClient, tal qual visto na documentação
+  componentDidMount() {
+    // this.pexelsClient = createClient(env.PEXELS_KEY)
+  }
 
   onBuscaRealizada = (termo) => {
-    console.log(termo)
+    pexelsClient.get('/search', {
+      params: {
+        query: termo,
+        per_page: 5  
+      }
+    }).then(result => this.setState({photos: result.data.photos}))
   }
-  render(){
-    console.log(env.PEXELS_KEY)
-    console.log(window.env.PEXELS_KEY)
+
+  // onBuscaRealizada = (termo) => {
+  //   this.pexelsClient.photos.search({
+  //     query: termo,
+  //     per_page: 5
+  //   }).then(result => this.setState({ photos: result.photos }))
+  // }
+  render() {
     return (
       <div
         className='grid justify-content-center'>
         <div className="col-12">
-          <h1>Exibir uma lista de...</h1>
+          <PexelsLogo />
+        </div>  
+        <div className="col-12">
+          <h1 className='text-center'>Exibir uma lista de...</h1>
         </div>
         <div className="col-12">
-          <Busca 
-            dica='Buscar imagens de...' 
-            onBuscaRealizada={this.onBuscaRealizada}/>
+          <Busca
+            dica='Buscar imagens de...'
+            onBuscaRealizada={this.onBuscaRealizada} />
+        </div>
+        <div className="col-12">
+          <ListaImagens photos={this.state.photos} />
         </div>
       </div>
     )
---
+++ exibe-fotos/src/components/ListaImagens.jsx
@@ -0,0 +1,13 @@
+// rafce
+import React from 'react'
+import Imagem from './Imagem'
+
+const ListaImagens = ({photos}) => {
+  return (
+    photos.map((photo) => (
+      <Imagem src={photo.src.small} alt={photo.alt}/>
+    ))
+  )
+}
+
+export default ListaImagens

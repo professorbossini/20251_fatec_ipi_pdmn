036030697d63ce0702eae101b29626c7e0ad01dc
Rodrigo Bossini <professorbossini@gmail.com>
2025-05-09 21:47:45 -0300

    learn(ap07-react): faz o back end
    - terminamos a apostila e fizemos um back end para isolar as chaves

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -8,6 +8,7 @@ import Imagem from './components/Imagem'
 import ListaImagens from './components/ListaImagens'
 import PexelsLogo from './components/PexelsLogo'
 
+
 class App extends React.Component {
 
   pexelsClient = null
@@ -25,7 +26,7 @@ class App extends React.Component {
     pexelsClient.get('/search', {
       params: {
         query: termo,
-        per_page: 5  
+        per_page: 15  
       }
     }).then(result => this.setState({photos: result.data.photos}))
   }
@@ -52,7 +53,9 @@ class App extends React.Component {
             onBuscaRealizada={this.onBuscaRealizada} />
         </div>
         <div className="col-12">
-          <ListaImagens photos={this.state.photos} />
+          <div className="grid">
+            <ListaImagens  imgStyle={"col-12 md:col-6 lg:col-4 xl:col-3"} photos={this.state.photos} />
+          </div>
         </div>
       </div>
     )
